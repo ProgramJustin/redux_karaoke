69d699bff59268a0d7e1ff9b088ee4d3f8f282b7
Justin Gomez <programjustin@gmail.com>
2018-03-05 12:35:41 -0800

    dispatch NEXT_LYRIC or RESTART_SONG depending on currentState in click listener

--- scripts.js
+++ scripts.js
@@ -66,8 +66,12 @@ window.onload = function() {
 
 // CLICK LISTENER
 const userClick = () => {
-  store.dispatch({ type: 'NEXT_LYRIC'} );
-  console.log(store.getState());
+  const currentState = store.getState();
+  if (currentState.arrayPosition === currentState.songLyricsArray.length -1) {
+    store.dispatch({ type: 'RESTART_SONG' } );
+  } else {
+    store.dispatch({ type: 'NEXT_LYRIC' } );
+  }
 }
 
 // SUBSCRIBE TO REDUX STORE
