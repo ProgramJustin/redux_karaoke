a3b361adc7a14d0170af19deaaacea410e7a64c1
Justin Gomez <programjustin@gmail.com>
2018-03-10 09:57:09 -0800

    update UI to display a list of available songs for users to select from

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 // LYRIC INFO
-const songLyricsArray = "My mama called, seen you on TV, son Said shit done changed ever since we was on I dreamed it all ever since I was young They said I wouldn't be nothing Now they always say congratulations Worked so hard, forgot how to vacation They ain't never had the dedication People hatin', say we changed and look we made it Yeah, we made it".split(', ');
 
 const songList = {
   1: "My mama called, seen you on TV, son Said shit done changed ever since we was on I dreamed it all ever since I was young They said I wouldn't be nothing Now they always say congratulations Worked so hard, forgot how to vacation They ain't never had the dedication People hatin', say we changed and look we made it Yeah, we made it".split(', '),
